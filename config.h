53c0dd77a5c3fb7953ce72bcf112d5c5b6009695
teraoka <teraoka@pop-os.localdomain>
2022-01-24 19:16:22 +0900

    Initial Commit

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifdef MOUSEKEY_TIME_TO_MAX
+   #undef MOUSEKEY_TIME_TO_MAX
+   #define MOUSEKEY_TIME_TO_MAX 16 
+#endif
+
+#ifdef MIDI_ADVANCED
+   #undef MIDI_ADVANCED
+   #define MIDI_ADVANCED
+#endif
