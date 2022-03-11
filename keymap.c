062929b53cd60d3fd9770c3917971224b39f4e53
teraoka-k <teraoka.kennosuke@gmail.com>
2022-03-11 19:01:06 +0900

    change rs to ra

--- keymap.c
+++ keymap.c
@@ -15,17 +15,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * | Alt  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Alt  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |      |      | Alt  | Lower|    Space    |Raise |  OS  |      |      | Enter|
+ * | Esc  |      |      | Shift| Lower|    Space    |Raise |  OS  |      |      | Enter|
  * `-----------------------------------------------------------------------------------'
  * 
  */
 [BASE] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_DEL,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_ESC,  _______, _______, KC_LALT, MO(LOWER),  KC_SPC, KC_SPC,  MO(RAISE), KC_LGUI, _______, _______, KC_ENT 
+    KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+    KC_ESC,  _______, _______, KC_LSFT, MO(LOWER),  KC_SPC, KC_SPC,  MO(RAISE), KC_LGUI, _______, _______, KC_ENT 
 ),
 
 /* Lower
